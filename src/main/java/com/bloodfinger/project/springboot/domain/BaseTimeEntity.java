10b44496fcb9054131065fd8643f9e6bb1b31f6f
bloodfinger <didwodn8822@gmail.com>
2020-04-17 23:42:14 +0900

    JPA Auditing 등록

---
+++ src/main/java/com/bloodfinger/project/springboot/domain/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package com.bloodfinger.project.springboot.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
+
+/* BaseTmeEntity 클래스는 모든 Entity의 상위클래스가 되어 Entity들의
+*   CreatedDate , modifiedDate를 자동으로 관리하는 역할입니다.
+* */
--- src/main/java/com/bloodfinger/project/springboot/domain/posts/Posts.java
+++ src/main/java/com/bloodfinger/project/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.bloodfinger.project.springboot.domain.posts;
 
+import com.bloodfinger.project.springboot.domain.BaseTimeEntity;
 import com.bloodfinger.project.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
