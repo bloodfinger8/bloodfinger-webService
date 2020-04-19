1c4731ce6df012d3c51643c0cbc1830f5fb68d47
bloodfinger <didwodn8822@gmail.com>
2020-04-19 21:50:47 +0900

    mustache 추가

---
+++ src/main/java/com/bloodfinger/project/springboot/web/IndexController.java
@@ -0,0 +1,18 @@
+package com.bloodfinger.project.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+}
--- src/test/java/com/bloodfinger/project/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/bloodfinger/project/springboot/domain/posts/PostsRepositoryTest.java
@@ -61,6 +61,8 @@ public class PostsRepositoryTest {
         System.out.println(">>> CreateDate =" + posts.getCreatedDate()
          + ", modifiedDate =" + posts.getModifiedDate() );
 
+
+
         assertThat(posts.getCreatedDate()).isAfter(now);
         assertThat(posts.getModifiedDate()).isAfter(now);
     }
