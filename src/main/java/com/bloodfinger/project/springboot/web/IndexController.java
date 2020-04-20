d1feb84555b8600db8cb168033a304a29d984114
bloodfinger <didwodn8822@gmail.com>
2020-04-20 22:46:14 +0900

    html css 추가

--- src/main/java/com/bloodfinger/project/springboot/web/IndexController.java
+++ src/main/java/com/bloodfinger/project/springboot/web/IndexController.java
@@ -1,11 +1,11 @@
 package com.bloodfinger.project.springboot.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class IndexController {
-
     @GetMapping("/")
     public String index(){
         return "index";
