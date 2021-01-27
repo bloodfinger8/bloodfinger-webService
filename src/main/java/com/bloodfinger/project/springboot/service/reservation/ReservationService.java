5dc23830f01298f7825af5cd4e5c3f4a6c36c5b6
bloodfinger <didwodn8822@gmail.com>
2021-01-27 15:38:19 +0900

    메일 서비스 주석처리

--- src/main/java/com/bloodfinger/project/springboot/service/reservation/ReservationService.java
+++ src/main/java/com/bloodfinger/project/springboot/service/reservation/ReservationService.java
@@ -18,29 +18,29 @@ public class ReservationService {
 
     private final ReservationRepository reservationRepository;
 
-    @Autowired
-    private JavaMailSender mailSender;
+//    @Autowired
+//    private JavaMailSender mailSender;
 
     @Transactional
     public Long save(ReservationSaveRequestDto reservationSaveRequestDto){
         return reservationRepository.save(reservationSaveRequestDto.toEntity()).getNo();
     }
 
-    @Transactional
-    public void sendEmail(ReservationSaveRequestDto reservationSaveRequestDto) throws MessagingException , IOException {
-        StringBuilder text = new StringBuilder();
-        text.append("예약자명 : " + reservationSaveRequestDto.getName() + "\r\n");
-        text.append("연락처 : " + reservationSaveRequestDto.getPhnNo()+ "\r\n");
-        text.append("예약날짜 : " + reservationSaveRequestDto.getDate()+ "\r\n");
-        text.append("예약자명 : " + reservationSaveRequestDto.getTime()+ "\r\n");
-        text.append("요청사항 : " + reservationSaveRequestDto.getComment());
-
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo("didwodn8822@gmail.com");
-        message.setFrom("didwodn8822@gmail.com");
-        message.setSubject(reservationSaveRequestDto.getDate() + " / 예약 신청");
-        message.setText(text.toString());
-
-        mailSender.send(message);
-    }
+//    @Transactional
+//    public void sendEmail(ReservationSaveRequestDto reservationSaveRequestDto) throws MessagingException , IOException {
+//        StringBuilder text = new StringBuilder();
+//        text.append("예약자명 : " + reservationSaveRequestDto.getName() + "\r\n");
+//        text.append("연락처 : " + reservationSaveRequestDto.getPhnNo()+ "\r\n");
+//        text.append("예약날짜 : " + reservationSaveRequestDto.getDate()+ "\r\n");
+//        text.append("예약자명 : " + reservationSaveRequestDto.getTime()+ "\r\n");
+//        text.append("요청사항 : " + reservationSaveRequestDto.getComment());
+//
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo("didwodn8822@gmail.com");
+//        message.setFrom("didwodn8822@gmail.com");
+//        message.setSubject(reservationSaveRequestDto.getDate() + " / 예약 신청");
+//        message.setText(text.toString());
+//
+//        mailSender.send(message);
+//    }
 }
