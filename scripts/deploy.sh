3d37e5271b1d95d3b2b0001b8b29e0b43da78662
bloodfinger <didwodn8822@gmail.com>
2021-02-18 00:13:29 +0900

    수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ echo "-> 현재 구동중인 애플리케이션 PID 확인중입니다."
 CURRENT_PID=$(pgrep -fl bloodfinger-webService | grep jar | awk '{print $1}')
 ehco "-> 현재 구동중인 애플리케이션 PID : $CURRENT_PID"
 
-if[ -z "$CURRENT_PID" ]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "-> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "-> kill -15 $CURRENT_PID"
